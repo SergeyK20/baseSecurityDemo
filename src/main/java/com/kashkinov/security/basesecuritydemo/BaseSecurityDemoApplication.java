166245d7a199b4ca0bb1e2e4c3ebc3846af988e3
SergeyK20 <kashkinov20@gmail.com>
2023-02-05 19:06:36 +0400

    first commit

---
+++ src/main/java/com/kashkinov/security/basesecuritydemo/BaseSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kashkinov.security.basesecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kashkinov/security/basesecuritydemo/BaseSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kashkinov.security.basesecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
